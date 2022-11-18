7493a169927db13afb30b5768e087e6b0167aa88
Augusto Tapia <atdevlop@gmail.com>
2022-11-17 20:08:39 -0600

    Actualiza zone_type

--- app/Traits/BodyJsonZipCode.php
+++ app/Traits/BodyJsonZipCode.php
@@ -25,14 +25,10 @@ trait BodyJsonZipCode{
             $sep = explode("|", $num);
             $row = array(
                 "key" => (int)$sep[12],
-                "name" => strtoupper(utf8_encode($sep[1])),
+                "name" => $this->eliminar_tildes($sep[1]),
                 "zone_type" => strtoupper($sep[13]),
                 "settlement_type" => array(
                     "name" => utf8_encode($sep[2])
-                ),
-                "municipality" => array(
-                    "key" => $sep[12],
-                    "name" => utf8_encode($sep[3])
                 )
                 
             );
@@ -42,15 +38,60 @@ trait BodyJsonZipCode{
         //Genera cabecera y adjunta cuerpo de datos
         $arrayJson = [
             "zip_code" => $divide[0],
-            "locality" => strtoupper(utf8_encode($divide[5])),
+            "locality" => $this->eliminar_tildes($divide[5]),
             "federal_entity" => [
                 "key" => $divide[7],
-                "name" => strtoupper(utf8_encode($divide[4])),
+                "name" => $this->eliminar_tildes($divide[4]),
                 "code" => null
             ],
-            "settlements" => $data
+            "settlements" => $data,
+            "municipality" => array(
+                "key" => $sep[12],
+                "name" => $this->eliminar_tildes($divide[3])
+            )
           ];
 
         return $arrayJson; 
+    }
+
+    function eliminar_tildes($cadena){
+
+        //Codificamos la cadena en formato utf8 en caso de que nos de errores
+        $cadena = utf8_encode($cadena);
+    
+        //Ahora reemplazamos las letras
+        $cadena = str_replace(
+            array('á', 'à', 'ä', 'â', 'ª', 'Á', 'À', 'Â', 'Ä'),
+            array('a', 'a', 'a', 'a', 'a', 'A', 'A', 'A', 'A'),
+            $cadena
+        );
+    
+        $cadena = str_replace(
+            array('é', 'è', 'ë', 'ê', 'É', 'È', 'Ê', 'Ë'),
+            array('e', 'e', 'e', 'e', 'E', 'E', 'E', 'E'),
+            $cadena );
+    
+        $cadena = str_replace(
+            array('í', 'ì', 'ï', 'î', 'Í', 'Ì', 'Ï', 'Î'),
+            array('i', 'i', 'i', 'i', 'I', 'I', 'I', 'I'),
+            $cadena );
+    
+        $cadena = str_replace(
+            array('ó', 'ò', 'ö', 'ô', 'Ó', 'Ò', 'Ö', 'Ô'),
+            array('o', 'o', 'o', 'o', 'O', 'O', 'O', 'O'),
+            $cadena );
+    
+        $cadena = str_replace(
+            array('ú', 'ù', 'ü', 'û', 'Ú', 'Ù', 'Û', 'Ü'),
+            array('u', 'u', 'u', 'u', 'U', 'U', 'U', 'U'),
+            $cadena );
+    
+        $cadena = str_replace(
+            array('ñ', 'Ñ', 'ç', 'Ç'),
+            array('n', 'N', 'c', 'C'),
+            $cadena
+        );
+    
+        return strtoupper($cadena);
     }
 }
